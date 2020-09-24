d9ea5a031b1c72e2080f1f74b5064b4b9fe9222d
ksangroula <ksangroula@gmail.com>
2020-09-24 18:04:21 +0300

    add practice file into gitghub

---
+++ es6-arrow-functions/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+
+
+// const newNumbers = numbers.map(x => x * 2);
+
+// console.log(newNumbers);
